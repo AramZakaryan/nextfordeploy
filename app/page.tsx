dbd96f898aeb3e61fd40c10498d5317add102556
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-22 00:43:55 +0400

    with db

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import {prisma} from "@/lib/prisma";
 
-export default function Home() {
-  return (
+export default async function Home() {
+  const mardiq = await prisma.mard.findMany()
+  return (<>
     <h1>next for deploy 1</h1>
+        <h2>{mardiq[0].name}</h2>
+      </>
   );
 }
